9866cda78fafbb4a29997ede14a99fe31c6d604b
Mirucow <miruchqn@gmail.com>
2024-11-14 01:43:58 +0900

    refactor endian_rw and variable_rw tests to use write_proto_from and read_proto_into methods

--- tests/tests/endian_rw.rs
+++ tests/tests/endian_rw.rs
@@ -8,14 +8,16 @@ fn endian_rw() {
     let mut buf = vec![];
 
     buf.write_proto(&BE::<i16>(-123)).unwrap();
-    buf.write_proto(&LE::<u32>(12345)).unwrap();
+    // you can write the type directly (requires From trait)
+    buf.write_proto_from::<u32, LE<u32>>(12345).unwrap();
     buf.write_proto(&BE::<i64>(-1234567)).unwrap();
     buf.write_proto(&LE::<u128>(123456789)).unwrap();
 
     let mut cursor = std::io::Cursor::new(&buf);
 
     assert_eq!(i16::from(cursor.read_proto::<BE<i16>>().unwrap()), -123);
-    assert_eq!(u32::from(cursor.read_proto::<LE<u32>>().unwrap()), 12345);
+    // you can read the type directly (requires Into trait)
+    assert_eq!(cursor.read_proto_into::<LE<u32>, u32>().unwrap(), 12345);
     assert_eq!(i64::from(cursor.read_proto::<BE<i64>>().unwrap()), -1234567);
     assert_eq!(
         u128::from(cursor.read_proto::<LE<u128>>().unwrap()),
--- tests/tests/variable_rw.rs
+++ tests/tests/variable_rw.rs
@@ -5,7 +5,8 @@ fn variable_rw() {
     let mut buf = vec![];
 
     buf.write_proto(&Var::<u16>(123)).unwrap();
-    buf.write_proto(&Var::<u32>(12345)).unwrap();
+    // you can write the type directly (requires From trait)
+    buf.write_proto_from::<u32, Var<u32>>(12345).unwrap();
     buf.write_proto(&Var::<u64>(1234567)).unwrap();
     buf.write_proto(&Var::<i16>(-123)).unwrap();
     buf.write_proto(&Var::<i32>(-12345)).unwrap();
@@ -14,7 +15,8 @@ fn variable_rw() {
     let mut cursor = std::io::Cursor::new(&buf);
 
     assert_eq!(u16::from(cursor.read_proto::<Var<u16>>().unwrap()), 123);
-    assert_eq!(u32::from(cursor.read_proto::<Var<u32>>().unwrap()), 12345);
+    // you can read the type directly (requires Into trait)
+    assert_eq!(cursor.read_proto_into::<Var<u32>, u32>().unwrap(), 12345);
     assert_eq!(u64::from(cursor.read_proto::<Var<u64>>().unwrap()), 1234567);
     assert_eq!(i16::from(cursor.read_proto::<Var<i16>>().unwrap()), -123);
     assert_eq!(i32::from(cursor.read_proto::<Var<i32>>().unwrap()), -12345);
