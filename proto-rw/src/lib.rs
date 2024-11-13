c98f932bda50e96db6728c6f5cc4875385edaa87
Mirucow <miruchqn@gmail.com>
2024-11-14 01:26:14 +0900

    add read_proto_into and write_proto_from methods to PRead and PWrite traits

--- proto-rw/src/lib.rs
+++ proto-rw/src/lib.rs
@@ -14,6 +14,10 @@ pub trait PRead: std::io::Read + Sized {
     fn read_proto<T: ProtoRw>(&mut self) -> Result<T, ProtoRwError> {
         Ok(T::read(self)?)
     }
+
+    fn read_proto_into<T: ProtoRw + Into<U>, U>(&mut self) -> Result<U, ProtoRwError> {
+        Ok(T::read(self)?.into())
+    }
 }
 
 impl<R: std::io::Read + Sized> PRead for R {}
@@ -21,6 +25,10 @@ impl<R: std::io::Read + Sized> PRead for R {}
 pub trait PWrite: std::io::Write + Sized {
     fn write_proto<T: ProtoRw>(&mut self, ty: &T) -> Result<(), ProtoRwError> {
         ty.write(self)
+    }
+
+    fn write_proto_from<T, U: ProtoRw + From<T>>(&mut self, ty: T) -> Result<(), ProtoRwError> {
+        U::from(ty).write(self)
     }
 }
 
